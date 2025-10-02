691996e8d60da74761feb16f96c4493bc85ae01d
Mario Reder <mario.reder@pm.me>
2025-10-02 20:53:21 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Decentralized Perpetual Trading Platform in the Diamond Hands Ecosystem",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Rewards,Vaults",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Rewards,Vaults",
   "VITE_CUSTOM_MENUS": "Purchase $VAL, https://raydium.io/swap/?inputMint=sol&outputMint=DtwN3PHiKEpK1MSekViVC8QifdRJCkML6HG16sXHpump; Play Diamond Hands, https://app.valmeme.com/",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/StoreofVal",
